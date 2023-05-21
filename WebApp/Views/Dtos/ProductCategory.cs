09d5234446c2e5cf61662d51cb24e276d2f30ee8
lova <thoma@LAPTOP-7SSJQT9U>
2023-05-21 12:38:13 +0200

    21_05_2

---
+++ WebApp/Views/Dtos/ProductCategory.cs
@@ -0,0 +1,8 @@
+﻿namespace WebApp.Models.Dtos
+{
+    public class ProductCategory
+    {
+        public int Id { get; set; }
+        public string CategoryName { get; set; } = null!;
+    }
+}
---
+++ WebApp/Views/ViewModels/ProductRegistrationViewModel.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+using WebApp.Models.Dtos;
+
+namespace WebApp.Models.ViewModels
+{
+    public class ProductRegistrationViewModel
+    {
+        [Display(Name = "Article Number")]
+        public string ArticleNumber { get; set; } = null!;
+
+        [Display(Name = "Product Name")]
+        public string ProductName { get; set; } = null!;
+
+        [Display(Name = "Product Description")]
+        public string? ProductDescription { get; set; }
+
+        [Display(Name = "Product Price (SEK)")]
+        public decimal ProductPrice { get; set; }
+
+        public int ProductCategoryId { get; set; }
+        public List<SelectListItem> ProductCategoryOptions { get; set; } = new List<SelectListItem>();
+
+
+
+
+
+        [Display(Name = "Tags (seperate by ; to add more then one tag)")]
+        public string? TagList { get; set; }
+        public List<string> Tags { get; set; } = new List<string>();
+
+
+        [Display(Name = "Product Image")]
+        [DataType(DataType.Upload)]
+        public IFormFile? ProductImageUpload { get; set; }
+    }
+}
